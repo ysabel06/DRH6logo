2093075713cde763f469c76033ad88db72ad67d7
ysabel06 <45562815+ysabel06@users.noreply.github.com>
2019-02-01 17:01:49 +0100

    Add files via upload

---
+++ contenu.php
@@ -0,0 +1,108 @@
+<div id="contenu" class="container-fluid">
+  <div id="contenu1" class="container-fluid">
+    <div class="page1">
+      <p>vous etes bien sur la page d'acceuil<br></p><br>
+      <p></p><br>
+      <p></p>
+      <section id="section">
+
+        <a href="https://www.denofgeek.com/uk/movies/justice-league/53559/justice-league-2-and-possible-ways-forward">
+        <img src="law.png">
+        
+      </a>
+        
+     </section>
+     <section id="section1">
+      <a href="http://www.lusocarrelage.fr/curriculum-vitae-francais-exemple-simple-les-exemples-des-cv-36287/">
+        <img src="4.png">
+      </a>
+       </section>
+
+    
+     <section id="section2">
+      <a href="https://www.alamy.com/prod-db-davis-filmsimpact-pictures-constantin-film-international-dr-resident-evil-retribution-de-paul-ws-anderson-2012-usaall-avec-milla-jovovich-fantastique-action-saga-sequelle-suite-femme-daction-horreur-science-fiction-dapres-le-jeu-video-du-mme-nom-image220010234.html">
+        <img src="3.png">
+      </a>
+     </section>
+
+     <section id="section3">
+      <a href="https://www.youtube.com/watch?v=J18zukKyDZs&index=9&list=LLKKiLvnXh0dEtl-aW1i27Nw&t=0s">
+        <img src="2.jpg">
+      </a>
+     </section>
+
+       <section id="section4">
+      <a href="http://www.unjourdereve.fr/ce-que-la-forme-de-votre-main-revele-sur-votre-personnalite/">
+        <img src="5.png">
+      </a>
+     </section>
+
+
+
+
+    </div>
+  </div>
+
+
+<div id="contenu2" class="container-fluid">
+
+          <div class="page2"> 
+            
+             
+                <h3>Expertise<span class="blue">.</span></h3><br>
+                <p>Simplon, c’est une équipe d’experts : la bonne personne au bon endroit
+                 pour une performance optimale.</p>                
+                 <h3>Expérience Utilisateur<span class="blue">.</span></h3><br>
+                 <p>Notre point fort : à l’écoute de l’utilisateur nous travaillons sans relâche à optimiser la solution Inside pour mieux répondre à vos attentes et enjeux. </p>
+                 <h3>Disponibilité et Empathie<span class="blue">.</span></h3><br>
+                 <p>Nous aimons notre métier et la solution que nous développons.
+                   Mais par-dessus-tout, nous aimons être utiles, et faciliter la vie de nos utilisateurs.</p>
+
+                   <img src="https://www.inside-reporting.fr/wp-content/uploads/2016/05/photo-societe-infineo-inside.png">  
+
+
+          </div>
+
+</div>
+
+<div id="contenu3" class="container-fluid">
+          <div class="page3">
+          <h2>L'Equipe de DRH-Inside</h2>
+          <img src="img/storm.png">
+<div class="membres">
+          <h3>Membres :</h3><br>
+          <a href="https://github.com/ysabel06">Ysabelle</a>
+          <br>
+          <a href="https://github.com/Staaz1990">Alexandre</a>
+          <br>
+          <a href="https://github.com/yooo83">Yoann</a>
+          <br>
+          <a href="https://github.com/JeremyViz">Jeremy</a>
+          <br>          </div>
+<div class="simplon">
+          <h3>Groupe Simplon :</h3><br>
+          <a href="https://simplonline.co/">Simplon.com</a>
+
+</div>
+</div>
+</div>
+    <div id="formulaire" class="page4">
+        <p>Remplissez les champs ci-dessous.</p>
+        <div class="row">
+        <form action="/action_page.php">
+          <label for="fname">Prenom :</label>
+            <input type="text" id="fname" name="firstname">
+          <label for="lname">Nom :</label>
+            <input type="text" id="lname" name="lastname">
+          <label for="Email">Adresse e-mail :</label>
+            <input type="text" id="Email" name="E-mail">
+          <label for="Sujet">Sujet :</label>
+            <input type="text" id="Sujet" name="Sujet">
+          <label for="message">Message :</label>
+            <textarea id="message" name="message"></textarea> 
+          <input type="submit" value="Envoyer">
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
